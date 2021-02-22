690f9c0d546fed205e58f44a5deeb8a10f295729
geno nullfree nullfree.geno@gmail.com <nullfree.geno@gmail.com>
2021-02-21 22:43:25 -0500

    Add test files

--- src/test.rs
+++ src/test.rs
@@ -3,19 +3,19 @@ use crate::tar::*;
 
 #[test]
 fn open_tar_file() {
-    file_read("1.tar".to_string());
+    file_read("test/1.tar".to_string());
 }
 
 #[test]
 fn write_tar_file() {
-    let mut data = file_read("1.txt".to_string());
+    let mut data = file_read("test/1.txt".to_string());
 
-    tar_write("2.tar".to_string(), &mut data);
+    tar_write("test/2.tar".to_string(), &mut data);
 }
 
 #[test]
 fn read_write_tar_file() {
-    let mut data = tar_read("1.tar".to_string());
+    let mut data = tar_read("test/1.tar".to_string());
 
-    tar_write("3.tar".to_string(), &mut data);
+    tar_write("test/3.tar".to_string(), &mut data);
 }
